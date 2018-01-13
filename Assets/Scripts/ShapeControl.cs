786f6b2ce3ab2af234c07b7ec5b4cb84301b524f
Alex-PC\Alex <lazeaai@gmail.com>
2018-01-13 15:28:43 -0500

    Gameplay setup

---
+++ Assets/Scripts/ShapeControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeControl : MonoBehaviour {
+
+    public float hSensitivity = 20f;
+    public float vSensitivity = 20f;
+
+    public bool xRotation = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if(xRotation)
+        {
+            transform.eulerAngles += Vector3.up * h * hSensitivity * Time.deltaTime;
+        } else
+        {
+            transform.eulerAngles += Vector3.forward * -v * vSensitivity * Time.deltaTime;
+        }
+    }
+}
