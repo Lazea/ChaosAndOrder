afe0cbcd66eecbcf49e90b8bf4ea14924e32e3c0
Alex-PC\Alex <lazeaai@gmail.com>
2018-01-14 02:29:52 -0500

    Basic camera follow

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    public Transform target;
+
+    public float followRate;
+    Vector3 refVel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newPosition = Vector3.SmoothDamp(transform.position, target.position, ref refVel, followRate);
+        newPosition.z = transform.position.z;
+        transform.position = newPosition;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,14 +10,22 @@ public class PlayerController : MonoBehaviour {
     public bool grounded = false;
     float g = -9.81f;
     float verticalSpeed;
+    public float terminalFallSpeed;
+
     public float jumpPower;
+    bool jumping = false;
 
     public bool blocking;
+
+    Transform cameratarget;
+    float cameratargetHeight;
 
     CharacterController cc;
 
 	// Use this for initialization
 	void Start () {
+        cameratarget = transform.Find("CameraTarget");
+
         cc = gameObject.GetComponent<CharacterController>();
 	}
 	
@@ -28,16 +36,43 @@ public class PlayerController : MonoBehaviour {
         bool jump = Input.GetKeyDown(KeyCode.Space);
         bool block = Input.GetKey(KeyCode.LeftShift);
 
+        // Flip Character in direction of motion
+        Vector3 playerScale = transform.localScale;
+        if(h > 0)
+        {
+            playerScale.x = 1;
+        } else if(h < 0)
+        {
+            playerScale.x = -1;
+        }
+
+        transform.localScale = playerScale;
+
+        // Ground check
         RaycastHit hit;
-        if(Physics.SphereCast(transform.position, 0.15f, Vector3.down, out hit, 0.45f))
+        if(Physics.SphereCast(transform.position, 0.14f, Vector3.down, out hit, 0.4f))
         {
             grounded = true;
+            jumping = false;
         } else
         {
+            if (grounded && !jumping)
+            {
+                verticalSpeed = 0f;
+                jumping = false;
+            }
+
             grounded = false;
         }
 
+        // Fall speed
         verticalSpeed += g * Time.deltaTime;
+        if (verticalSpeed < terminalFallSpeed)
+        {
+            verticalSpeed = terminalFallSpeed;
+        }
+
+        // Character movement and control flow
         if (grounded)
         {
             if (block)
@@ -48,6 +83,9 @@ public class PlayerController : MonoBehaviour {
             else if (jump)
             {
                 verticalSpeed = jumpPower;
+
+                grounded = false;
+                jumping = true;
             }
             else
             {
@@ -56,6 +94,7 @@ public class PlayerController : MonoBehaviour {
             }
         }
 
+        // Apply movement
         cc.Move(transform.right * h * moveSpeed * Time.deltaTime);
         cc.Move(transform.up * verticalSpeed * Time.deltaTime);
     }
